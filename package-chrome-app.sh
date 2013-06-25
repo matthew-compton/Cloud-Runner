d4d8660ea2e1b512be69374e6adca8d74ba49bb6
Ambergleam <ambergleam@gmail.com>
2013-06-25 18:02:29 -0400

    Fixed icon problem

--- package-chrome-app.sh
+++ package-chrome-app.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-zip CloudRunner-ChromeApp *
+
+# The desired zip file name
+name=CloudRunner-ChromeApp.zip
+
+# Check if zip file already exists
+if [ -f $name ]; then
+	# If so, delete it
+	rm CloudRunner-ChromeApp.zip
+fi
+
+# zip the files together
+zip -r CloudRunner-ChromeApp .
